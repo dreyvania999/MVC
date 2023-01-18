34aa208e6ac9c229b2394915155375df68c884ea
Иван Александрович Дрей <83556084+dreyvania999@users.noreply.github.com>
2023-01-18 12:30:34 +0300

    Все сделал

--- WpfApp3/MainWindow.xaml.cs
+++ WpfApp3/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfApp3
 {
@@ -23,6 +11,21 @@ namespace WpfApp3
         public MainWindow()
         {
             InitializeComponent();
+            Model.ArifmeticSign = ArifmeticOperation;
+            Model.ResultText = Result;
+
+            CBOperation.SelectedIndex = 0;
+        }
+
+
+        private void CalculateResult_Click(object sender, RoutedEventArgs e)
+        {
+            Model.SetResult(CBOperation.SelectedIndex, FirstOperand.Text, SecondOperand.Text);
+        }
+
+        private void CBOperation_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Model.Operation = CBOperation.SelectedIndex;
         }
     }
 }
---
+++ WpfApp3/Model.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace WpfApp3
+{
+    internal class Model
+    {
+        private static readonly string[] Sign = new string[] { "+", "-", "/", "x" };
+
+        public static TextBlock ArifmeticSign;
+        public static TextBox ResultText;
+
+        public static int Operation
+        {
+            set => ArifmeticSign.Text = Sign[value];
+        }
+
+        public static void SetResult(int index, string FirstOperand, string SecondOperand)
+        {
+            try
+            {
+                double one = Convert.ToDouble(FirstOperand);
+                double two = Convert.ToDouble(SecondOperand);
+
+                ResultText.Text = index == 2 && two == 0 ? "На ноль разделить нельзя" : index switch
+                {
+                    0 => (one + two).ToString(),
+                    1 => (one - two).ToString(),
+                    2 => (one / two).ToString(),
+                    3 => (one * two).ToString(),
+                    _ => "Ошибка",
+                }; 
+            }
+            catch
+            {
+                ResultText.Text = "Проверьте введенные данные";
+                return;
+            }
+        }
+
+       
+    }
+}
